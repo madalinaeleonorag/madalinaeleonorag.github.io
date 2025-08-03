febd88649ba852eb5dbb13a3b8e1318c0747d18d
Madalina-Eleonora Gheorghe <madalinaeleonora.gheorghe@gmail.com>
2025-08-03 15:03:44 +0300

    new try for deploy

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,10 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 import { Icon } from './components/icon/icon';
 import { WORK_EXPERIENCES } from '../assets/experience';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Icon],
+  imports: [Icon],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
