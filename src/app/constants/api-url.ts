6200841c8c17569141bfca06ad7f5e7b809fd17f
Madalina-Eleonora Gheorghe <madalinaeleonora.gheorghe@gmail.com>
2025-12-23 11:14:51 +0200

    going back to the old implementation until a solution is found for git projects

--- src/app/constants/api-url.ts
+++ src/app/constants/api-url.ts
@@ -1,3 +1,5 @@
 export const MEDIUM_API_URL =
   'https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@madalinaeleonorag';
 export const GITHUB_GRAPHQL_URL = 'https://api.github.com/graphql';
+export const GITHUB_API_URL =
+  'https://api.github.com/users/madalinaeleonorag/repos?type=public&per_page=50';
--- src/app/services/external.service.ts
+++ src/app/services/external.service.ts
@@ -1,10 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { map } from 'rxjs/operators';
-import { environment } from '../../environments/environment';
-import { GithubProjectsResponse } from '../models/github-project.model';
-import { GET_PINNED_GITHUB_REPOSITORIES } from '../components/graphql/getPinnedGithubRepositories';
-import { GITHUB_GRAPHQL_URL, MEDIUM_API_URL } from '../constants/api-url';
+import { filter, map, tap } from 'rxjs/operators';
+import { GITHUB_API_URL, MEDIUM_API_URL } from '../constants/api-url';
+import { GithubProject } from '../models/github-project.model';
 
 @Injectable({
   providedIn: 'root',
@@ -12,22 +10,45 @@ import { GITHUB_GRAPHQL_URL, MEDIUM_API_URL } from '../constants/api-url';
 export class ExternalService {
   private http = inject(HttpClient);
 
+  // TODO fix this to get only pinned repositories using GraphQL API - currently not working due to authentication issues
+  // get githubProjects() {
+  //   return this.http
+  //     .post<GithubProjectsResponse>(
+  //       GITHUB_GRAPHQL_URL,
+  //       { query: GET_PINNED_GITHUB_REPOSITORIES },
+  //       {
+  //         headers: {
+  //           Authorization: `Bearer ${environment.githubToken}`,
+  //         },
+  //       }
+  //     )
+  //     .pipe(
+  //       map((response: GithubProjectsResponse) => {
+  //         return response.data.user.pinnedItems.nodes;
+  //       })
+  //     );
+  // }
+
   get githubProjects() {
-    return this.http
-      .post<GithubProjectsResponse>(
-        GITHUB_GRAPHQL_URL,
-        { query: GET_PINNED_GITHUB_REPOSITORIES },
-        {
-          headers: {
-            Authorization: `Bearer ${environment.githubToken}`,
-          },
-        }
+    const IDs = [
+      124085630, 206401229, 312567859, 181836705, 157966192, 311027833,
+    ];
+
+    return this.http.get<any>(GITHUB_API_URL).pipe(
+      map((response) => response.filter((item: any) => IDs.includes(item.id))),
+      map((objectItem: any) =>
+        objectItem.map(
+          (item: any): GithubProject => ({
+            name: item.name,
+            description: item.description,
+            languages: { nodes: [{ name: item.language, color: '' }] },
+            url: item.url,
+            openGraphImageUrl: `https://raw.githubusercontent.com/${item.full_name}/${item.default_branch}/demo.png`,
+            homepageUrl: item.homepage,
+          })
+        )
       )
-      .pipe(
-        map((response: GithubProjectsResponse) => {
-          return response.data.user.pinnedItems.nodes;
-        })
-      );
+    );
   }
 
   get mediumArticles() {
